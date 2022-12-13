44e77c8be823597993b76b7fd382436c134d9014
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-12-13 18:45:03 +0000

    Print the list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 3-print_reversed_list_integer.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
+def print_reversed_list_integer(my_list=[]):
+    """Print all integers of a list in reverse order."""
+    if isinstance(my_list, list):
+        for i in range(len(my_list) - 1, 0, -1):
+            print("{:d}".format(my_list[i]))
