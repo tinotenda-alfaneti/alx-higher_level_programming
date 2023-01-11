15110641325ea1c38c99d5f0212d0dabe037f3bb
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2023-01-11 23:44:28 +0000

    Fix documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+# 0-lookup.py
+"""Defines an object methods and attributes lookup function."""
+
 
 def lookup(obj):
-    """Return a list of methods and attributes."""
+    """Return a list of object's methods and attributes."""
     return (dir(obj))
