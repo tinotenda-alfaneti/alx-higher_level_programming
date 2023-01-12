0e47be06613267955b05cf99a3935d6beff59e87
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2023-01-12 00:37:28 +0000

    Add project 4 - check if an object is an instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-is_kind_of_class.py
+"""Defines a class and inherited class-checking function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if an object is an instance or inherited instance of a class."""
+    return isinstance(obj, a_class)
