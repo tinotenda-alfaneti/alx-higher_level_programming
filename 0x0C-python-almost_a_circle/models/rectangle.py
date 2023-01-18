19a1eedf0e3b37fdc3f4e4d48e441c4ab89be33d
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2023-01-18 14:13:35 +0000

    Add class update task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,23 @@ class Rectangle(Base):
         xy = f"{self.x}/{self.y}"
         widhig = f"{self.width}/{self.height}"
         return f"[Rectangle] ({self.id}) {xy} - {widhig}"
+
+    def update(self, *args):
+        """Update the Rectangle."""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
