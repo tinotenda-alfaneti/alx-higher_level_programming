a318eaf12c81893b0415eae52a237b7b03a75820
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2023-01-18 14:28:29 +0000

    Create square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# square.py
+
+"""square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square."""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string representation of a Square."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
