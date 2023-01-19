bbcca475173ededcbf3871fd62e59e2f1cc2746b
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2023-01-19 15:48:39 +0000

    Fix bug

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -60,7 +60,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """Return a list of classes instantiated from a file of JSON strings."""
+        """list of classes instantiated from a file of JSON strings."""
         filename = str(cls.__name__) + ".json"
         try:
             with open(filename, "r") as jsonfile:
